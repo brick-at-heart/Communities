0a5d51d9e3656677ec2f1a743e868ad76e75822b
Dave Snyder <87585510+MKEDataGuy@users.noreply.github.com>
2022-06-04 13:13:20 -0500

    Scaffold Communities

---
+++ src/Communities/Areas/User/Data/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BrickAtHeart.Communities.Areas.User.Data;
+
+// Add profile data for application users by adding properties to the User class
+public class CommunitiesUser : IdentityUser
+{
+}
---
+++ src/Communities/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrickAtHeart.Communities.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
